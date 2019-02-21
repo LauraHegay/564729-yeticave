af0c31bc8d57a5231380f62e37dec00d111c0dfc
LauraHegay <sashanika79@gmail.com>
2019-02-21 12:58:56 +0300

    Добавила сценарий и шлаблон для страницы лота

--- lot.php
+++ lot.php
@@ -1,34 +1,35 @@
 <?php
 require_once('functions.php'); //подключаем сценарий с функцией-шаблонизатором
-require_once('data.php'); //подключаем сценарий с данными
 $con= mysqli_init();
 mysqli_options($con, MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
 mysqli_real_connect($con, "localhost", "root", "", "yeticave_db");
 if ($con === false) {
     exit("Ошибка подключения: " . mysqli_connect_error());
 }
-    mysqli_set_charset($con, "utf8");
-    $sql = "SELECT categories.name FROM categories";
-    $sql_lots = "SELECT lots.id as id_lot, title, start_price, image_path, categories.name, ifnull(max(rates.sum_price),lots.start_price) FROM lots
+mysqli_set_charset($con, "utf8");
+if (isset($_GET['id'])) {
+    $id_lot=$_GET['id'];
+}
+else {
+    http_response_code(404);
+}
+$sql = "SELECT categories.name FROM categories";
+$sql_lots = "SELECT title, start_price, image_path, categories.name as category, ifnull(max(rates.sum_price),lots.start_price) as current_price, description FROM lots
 JOIN categories ON lots.category_id=categories.id
 LEFT JOIN rates ON lots.id=rates.id_lot
-WHERE lots.date_end >CURRENT_DATE()
-group by lots.id
-ORDER BY lots.date_create DESC
-LIMIT 9";
-    $result = mysqli_query($con, $sql);
-    $result_lots = mysqli_query($con, $sql_lots);
-    $cat = object_in_array($result, $con);
-    $lots=object_in_array($result_lots, $con);
-
+WHERE lots.id =$id_lot";
+$result = mysqli_query($con, $sql);
+$result_lots = mysqli_query($con, $sql_lots);
+$cat = object_in_array($result, $con);
+$lots=object_in_array($result_lots, $con);
 $is_auth = rand(0, 1);
 $user_name = 'Лаура'; // укажите здесь ваше имя
 
-$page_content = include_template('index.php', [
+$page_content = include_template('lot.php', [
     'categories'=>$categories,
-    'advertisements'=>$lots]);
+    'lot'=>$lots]);
 $layout_content = include_template('layout.php', [
-    'title' => 'Yeti - Главная страница',
+    'title' => 'Yeti - Страница каталога',
     'is_auth' => $is_auth,
     'user_name' => $user_name,
     'content' => $page_content,
@@ -36,5 +37,3 @@ $layout_content = include_template('layout.php', [
 ]);
 
 print($layout_content);
-
-
