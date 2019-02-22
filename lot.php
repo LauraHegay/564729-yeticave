83f30ac23cb4c432922959d4f5dbbab7a5ca4b26
LauraHegay <sashanika79@gmail.com>
2019-02-22 17:06:04 +0300

    Исправление ошибок 2

--- lot.php
+++ lot.php
@@ -8,11 +8,12 @@ if ($con === false) {
 }
 mysqli_set_charset($con, "utf8");
 if (isset($_GET['id'])) {
-    $id_lot=$_GET['id'];
+    $id_lot=intval($_GET['id']);
 }
 else {
     http_response_code(404);
     header("Location: http:pages/404.html");
+    exit();
 }
 $sql = "SELECT categories.name FROM categories";
 $sql_lots = "SELECT title, start_price, image_path, categories.name as category, ifnull(max(rates.sum_price),lots.start_price) as current_price, description FROM lots
@@ -20,10 +21,12 @@ JOIN categories ON lots.category_id=categories.id
 LEFT JOIN rates ON lots.id=rates.id_lot
 WHERE lots.id =$id_lot";
 $result = mysqli_query($con, $sql);
-$result_lots = mysqli_query($con, $sql_lots);
 $cat = object_in_array($result, $con);
-if ($result) {
-    $lots=mysqli_fetch_assoc($result_lots);
+$result_lots = mysqli_query($con, $sql_lots);
+$lots=mysqli_fetch_assoc($result_lots);
+if (is_null($lots['title'])) {
+    header("Location: http:pages/404.html");
+    exit();
 }
 $is_auth = rand(0, 1);
 $user_name = 'Лаура'; // укажите здесь ваше имя
