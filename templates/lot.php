f9e6b73c4e3dcbdc378336bad3b3ce60d9c19939
LauraHegay <sashanika79@gmail.com>
2019-03-04 23:36:02 +0300

    Update lot.php

--- templates/lot.php
+++ templates/lot.php
@@ -36,7 +36,7 @@
                     <form class="lot-item__form" action="https://echo.htmlacademy.ru" method="post">
                       <p class="lot-item__form-item form__item form__item--invalid">
                         <label for="cost">Ваша ставка</label>
-                        <input id="cost" type="text" name="cost" placeholder="12 000">
+                        <input id="cost" type="text" name="cost" placeholder="<?=$lot['step_rate']; ?>">
                         <span class="form__error">Введите наименование лота</span>
                       </p>
                       <button type="submit" class="button">Сделать ставку</button>
