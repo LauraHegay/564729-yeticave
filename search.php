662753b9c38478a477a792aa445cc89eb061cc83
LauraHegay <sashanika79@gmail.com>
2019-03-10 21:43:02 +0300

    Пагинация

--- search.php
+++ search.php
@@ -5,24 +5,36 @@ require_once('init.php');
 
 if (isset($_GET['search'])){
     $search = trim($_GET['search']);
+    $cur_page = $_GET['page'] ?? 1;
+    $page_items = 3; //число лотов на одной странице
     if ($search) {
+        $sql_count = "SELECT * FROM lots
+    WHERE MATCH(title, description) AGAINST(?)";
+        $stmt = db_get_prepare_stmt($con, $sql_count, [$search]);
+        mysqli_stmt_execute($stmt);
+        $res = mysqli_stmt_get_result($stmt);
+        $lots_count=mysqli_num_rows($res);
+        $page_count = ceil($lots_count / $page_items);
+        $offset = ($cur_page - 1) * $page_items;
+        $pages = range(1, $page_count);
         $sql = "SELECT lots.id as id_lot, title, start_price, image_path, categories.name, ifnull(max(rates.sum_price),lots.start_price) FROM lots
     JOIN categories ON lots.category_id=categories.id
     LEFT JOIN rates ON lots.id=rates.id_lot
     WHERE MATCH(title, description) AGAINST(?)
     group by lots.id
-    ORDER BY lots.date_create DESC;";
-
+    ORDER BY lots.date_create DESC LIMIT ".$page_items." OFFSET ".$offset;
         $stmt = db_get_prepare_stmt($con, $sql, [$search]);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
-
         $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
         if (!empty($lots)){
             $page_content = include_template('search.php', [
                 'categories' => $cat,
                 'advertisements' => $lots,
-                'search'=>$search
+                'search'=>$search,
+                'pages' => $pages,
+                'page_count' => $page_count,
+                'cur_page' => $cur_page
             ]);
         } else {
             $message= "Ничего не найдено по вашему запросу";
--- templates/search.php
+++ templates/search.php
@@ -20,20 +20,21 @@
     </section>
   <?php if($page_count>1):?>
     <ul class="pagination-list">
+        <?php if($cur_page !== 1) : ?>
+      <li class="pagination-item pagination-item-prev"><a href="search.php/?search=<?=$search; ?>&page=<?=($cur_page-1); ?>">Назад</a></li>
+        <?php else: ?>
+          <li class="pagination-item pagination-item-prev"><a>Назад</a></li>
+        <?php endif; ?>
       <?php foreach ($pages as $page): ?>
-        <li class="pagination-item pagination-item-prev"><a href="search.php/?page<?= --$cur_page; ?>">Назад</a></li>
-        <li class="pagination-item <?php if ($page=$cur_page): ?>pagination-item-active<?php endif; ?>">
-          <a href="search.php/?page<?=$page; ?>"><?=$page; ?></a>
+        <li class="pagination-item <?php if ($page==$cur_page): ?>pagination-item-active<?php endif; ?>">
+          <a href="/search.php/?search=<?=$search; ?>&page=<?=$page; ?>"><?=$page; ?></a>
         </li>
       <?php endforeach; ?>
+        <?php if($cur_page !== $page_count) : ?>
+          <li class="pagination-item pagination-item-next"><a href="search.php/?search=<?=$search; ?>&page=<?=($cur_page+1); ?>">Вперед</a></li>
+        <?php else: ?>
+          <li class="pagination-item pagination-item-next"><a>Вперед</a></li>
+        <?php endif; ?>
     </ul>
   <?php endif; ?>
-<!--    <ul class="pagination-list">-->
-<!--        <li class="pagination-item pagination-item-prev"><a>Назад</a></li>-->
-<!--        <li class="pagination-item pagination-item-active"><a>1</a></li>-->
-<!--        <li class="pagination-item"><a href="#">2</a></li>-->
-<!--        <li class="pagination-item"><a href="#">3</a></li>-->
-<!--        <li class="pagination-item"><a href="#">4</a></li>-->
-<!--        <li class="pagination-item pagination-item-next"><a href="#">Вперед</a></li>-->
-<!--    </ul>-->
 </div>
